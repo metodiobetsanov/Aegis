698890679bf964a6cdefbaa48f62738d73d3b2c0
Metodi Obetsanov <htc.sens1@gmail.com>
2023-01-09 15:12:11 +0200

    AEG-7 Setup Application UI theme and dependencies (#8)
    Signed-off-by: Metodi Obetsanov <htc.sens1@gmail.com>
    Co-authored-by: Metodi Obetsanov <metodi.obetsanov@e-hps.com>

---
+++ src/Aegis/gulpfile.js
@@ -0,0 +1,44 @@
+﻿const del = require('del');
+const gulp = require('gulp');
+const filter = require('gulp-filter');
+const npmdist = require('gulp-npm-dist');
+const rename = require('gulp-rename');
+
+const paths = {
+    base: {
+        base: {
+            dir: './'
+        },
+        node: {
+            dir: './node_modules'
+        }
+    },
+    src: {
+        base: {
+            dir: './wwwroot/',
+            files: './wwwroot/**/*'
+        },
+        libs: {
+            dir: './wwwroot/assets/libs',
+            files: './wwwroot/assets/libs/**/*'
+        }
+    }
+};
+
+gulp.task('copy:libs', function () {
+    const f = filter(['*.min.js', '**/*.min.js', '*.min.css', '**/*.min.css']);
+    return gulp
+        .src(npmdist(), { base: paths.base.node.dir, sourcemaps: true })
+        .pipe(f)
+        .pipe(rename(function (path) {
+            if (path.dirname.includes('build')) {
+                path.dirname = path.dirname.replace(/\/build/, '').replace(/\\build/, '');
+            }
+            else if (path.dirname.includes('dist')) {
+                path.dirname = path.dirname.replace(/\/dist/, '').replace(/\\dist/, '');
+            }
+        }))
+        .pipe(gulp.dest(paths.src.libs.dir, { sourcemaps: '.' }));
+});
+
+gulp.task('default', gulp.series('copy:libs'));
