58ebdfee81d9b1d23a6571204169b880855388b3
Metodi Obetsanov <htc.sens1@gmail.com>
2023-01-16 17:13:00 +0200

    AEG-15 Fix bugs and issues reported by SonarScans (#15)

--- src/Aegis.Persistence/Attributes/SecureColumnAttribute.cs
+++ src/Aegis.Persistence/Attributes/SecureColumnAttribute.cs
@@ -2,6 +2,11 @@
 {
 	using System;
 
+	/// <summary>
+	/// Secure Column Attribute
+	/// </summary>
+	/// <seealso cref="System.Attribute" />
+	[AttributeUsage(AttributeTargets.Property)]
 	internal sealed class SecureColumnAttribute : Attribute
 	{
 	}
