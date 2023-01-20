2f3721fc059dee8479d73aa480b12e11ad562812
Metodi Obetsanov <htc.sens1@gmail.com>
2023-01-20 18:59:45 +0200

    AEG-20 add unit tests

---
+++ tests/Aegis.UnitTests/Application/Validators/Commands/Auth/ConfirmEmailQueryValidatorTest.cs
@@ -0,0 +1,49 @@
+﻿namespace Aegis.UnitTests.Application.Validators.Commands.Auth
+{
+	using FluentValidation.Results;
+
+	using global::Aegis.Application.Queries.Auth;
+	using global::Aegis.Application.Validators.Commands.Auth;
+
+	public class ConfirmEmailQueryValidatorTests
+	{
+		public static TheoryData<ConfirmEmailQuery> ConfirmEmailQueryValues => new TheoryData<ConfirmEmailQuery>()
+		{
+			{ new ConfirmEmailQuery() },
+			{ new ConfirmEmailQuery { UserId = "test" } },
+			{ new ConfirmEmailQuery { Token = "test" } },
+			{ new ConfirmEmailQuery { UserId = "", Token = "test" }},
+			{ new ConfirmEmailQuery { UserId = "   ", Token = "test" }},
+			{ new ConfirmEmailQuery { UserId = "test", Token = "" }},
+			{ new ConfirmEmailQuery { UserId = "test", Token = "   " }},
+		};
+
+		[Fact]
+		public void Validate_ShouldBeTrue()
+		{
+			// Arrange
+			ConfirmEmailQuery query = new ConfirmEmailQuery { UserId = "test", Token = "test" };
+			ConfirmEmailQueryValidator validator = new ConfirmEmailQueryValidator();
+
+			// Act
+			ValidationResult result = validator.Validate(query);
+
+			// Assert
+			result.IsValid.ShouldBeTrue();
+		}
+
+		[Theory]
+		[MemberData(nameof(ConfirmEmailQueryValues))]
+		public void Validate_ShouldBeFalse_OnWrongData(ConfirmEmailQuery query)
+		{
+			// Arrange
+			ConfirmEmailQueryValidator validator = new ConfirmEmailQueryValidator();
+
+			// Act
+			ValidationResult result = validator.Validate(query);
+
+			// Assert
+			result.IsValid.ShouldBeFalse();
+		}
+	}
+}
---
+++ tests/Aegis.UnitTests/Application/Validators/Commands/Auth/EmailConfirmationQueryValidatorTest.cs
@@ -0,0 +1,46 @@
+﻿namespace Aegis.UnitTests.Application.Validators.Commands.Auth
+{
+	using FluentValidation.Results;
+
+	using global::Aegis.Application.Queries.Auth;
+	using global::Aegis.Application.Validators.Commands.Auth;
+
+	public class EmailConfirmationQueryValidatorTests
+	{
+		public static TheoryData<EmailConfirmationQuery> EmailConfirmationQueryValues => new TheoryData<EmailConfirmationQuery>()
+		{
+			{ new EmailConfirmationQuery() },
+			{ new EmailConfirmationQuery { UserId = "test" } },
+			{ new EmailConfirmationQuery { UserId = "" } },
+			{ new EmailConfirmationQuery { UserId = "   "} }
+		};
+
+		[Fact]
+		public void Validate_ShouldBeTrue()
+		{
+			// Arrange
+			EmailConfirmationQuery query = new EmailConfirmationQuery { UserId = "test" };
+			EmailConfirmationQueryValidator validator = new EmailConfirmationQueryValidator();
+
+			// Act
+			ValidationResult result = validator.Validate(query);
+
+			// Assert
+			result.IsValid.ShouldBeTrue();
+		}
+
+		[Theory]
+		[MemberData(nameof(EmailConfirmationQueryValues))]
+		public void Validate_ShouldBeFalse_OnWrongData(EmailConfirmationQuery query)
+		{
+			// Arrange
+			EmailConfirmationQueryValidator validator = new EmailConfirmationQueryValidator();
+
+			// Act
+			ValidationResult result = validator.Validate(query);
+
+			// Assert
+			result.IsValid.ShouldBeFalse();
+		}
+	}
+}
